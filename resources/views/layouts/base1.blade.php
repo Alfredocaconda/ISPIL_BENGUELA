281afd7d749f0220b724a9bd6cca5bd26a981e55
Alfredocaconda <123181679+Alfredocaconda@users.noreply.github.com>
2025-01-17 15:43:17 -0300

    trabalhando na pagina inicial

---
+++ resources/views/layouts/base1.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ISPM-BENGUELA</title>
+    <link rel="stylesheet" href="{{ asset('Css/estilo.css') }}">
+</head>
+<body>
+    <header class="cabecalho">
+        <div class="esquerdo">
+            <a href="{{ url('/') }}"><img src="{{ asset('imagem/1.jpg') }}" alt="">
+            </a>
+        </div>
+        <div class="direito">
+            <ul class="nav">
+                <li><a href="">Cursos</a></li>
+                <li><a href="">Vida Academica</a></li>
+                <li><a href="">Actividades</a></li>
+                <li><a href="">Ensino e Investigação</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+
+        </div>
+    </header>
+    <div class="fundo">
+            <div class="info">
+                <h1>CHEGOU A HORA DE FAZER</h1>
+                 <h1>PARTE DA FAMÍLIA ISPM-BENGUELA</h1>
+                 <p>INSCRIÇÕES ABERTAS PARA</p>
+                 <p>EXAMES DE ACESSO E CURSO PREPARATÓRIO</p>
+                 <h2>24 DE JULHO DE 2025</h2>
+                 <h2>WWW.ISPM_BENGUELA.AO</h2>
+                <h4>Candidatar-se</h4>
+                <p>Faça já a sua candidatura e venha concorrer por uma vaga no </p>
+                <p>Instituto Superior Politecnico Maravilha-Benguela</p>
+                    <a href=""><button>Aplicar</button></a>
+            </div>
+        
+    </div>
+    <div class="barra">
+        <h1>Actividades</h1>
+    </div>
+   <div class="actividade">
+    <ul id="actividade">
+        <li><img src="{{ asset('imagem/ACTIVIDADES/eng.jpg') }}" alt=""></li>
+        <li>primeiro</li>
+        <li>primeiro</li>
+        <li>primeiro</li>
+    </ul>
+   </div>
+</body>
+</html>
