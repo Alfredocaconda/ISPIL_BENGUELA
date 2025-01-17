80bc4b0c9a3dfd1fd95fb30f2e39ca1fcb3c5c5e
Alfredocaconda <123181679+Alfredocaconda@users.noreply.github.com>
2025-01-17 04:42:39 -0300

    trabalhando no index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ISMP-Inicial</title>
-</head>
-<body>
+@extends('layouts.base')
 
-  <h1><a href="{{route('inscricao.index')}}">CANDIDATAR-SE</a></h1>
-</body>
-</html>
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ISPM-BENGUELA</title>
+    <link rel="stylesheet" href="{{ asset('Css/style.css') }}">
+</head>
+<body>
+    <header class="cabecalho">
+        <div class="esquerdo">
+            <a href="{{ url('/') }}"><img src="{{ asset('imagem/1.jpg') }}" alt="">
+            </a>
+        </div>
+        <div class="direito">
+            <ul>
+                <li><a href="">Cursos</a></li>
+                <li><a href="">Vida Academica</a></li>
+                <li><a href="">Actividades</a></li>
+                <li><a href="">Ensino e Investigação</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="fundo">
+        <div class="info">
+            <h4>Candidatar-se</h4>
+            <p>Faça já a sua candidatura e venha concorrer por uma vaga no </p>
+            <p>Instituto Superior Politecnico Maravilha-Benguela</p>
+            <div class="botao">
+                <a href=""><h6>Aplicar</h6></a>
+            </div>
+        </div>
+    </div>
+    
+   <div class="actividade">
+    <ul>
+        <li>primeiro</li>
+        <li>primeiro</li>
+        <li>primeiro</li>
+        <li>primeiro</li>
+    </ul>
+   </div>
+</body>
+</html>
